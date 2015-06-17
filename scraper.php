65d050016f82df789c4efb13cccfb645a258baa9
sukelali <sukelali@gmail.com>
2015-06-17 11:48:27 +0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,4 +1,6 @@
 <?
+$html = scraperWiki::scrape("http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm");           
+print $html . "\n";
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
